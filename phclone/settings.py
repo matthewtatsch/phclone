1710676a71cf48881659a970e326f32792f84262
Matthew Tatsch <matt.tatsch@gmail.com>
2018-06-09 09:48:51 -0500

    static files

--- phclone/settings.py
+++ phclone/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'phclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'phclone/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'phclone/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
