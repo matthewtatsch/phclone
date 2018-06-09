c2abf016ca2e7d464296cdc1fc2265dcd2e8efba
Matthew Tatsch <matt.tatsch@gmail.com>
2018-06-09 12:17:38 -0500

    authorization

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        # The user is registering a new account
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/register.html',
+                              {'error': "Username already in use"})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'],
+                    password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/register.html',
+                          {'error': "Passwords do not match"})
+    else:
+        # User wants to enter info
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],
+                                 password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html',
+                          {'error': "Username or Password is incorrect."})
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    # TODO need to route to home page
+    # TODO don't forget to log out
+    return render(request, 'accounts/logout.html')
--- phclone/urls.py
+++ phclone/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
 ]
