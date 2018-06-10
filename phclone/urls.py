7a5248e9a0003b201b4b4a7a2ecf7aec94a5ff8f
Matthew Tatsch <matt.tatsch@gmail.com>
2018-06-09 23:09:31 -0500

    create Product functionality

--- phclone/urls.py
+++ phclone/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from products import views
@@ -22,4 +24,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
